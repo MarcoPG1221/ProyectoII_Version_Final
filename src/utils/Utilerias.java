b4934783da9f761a769a3e80693cdaf9f3b98c53
Marco Perez <antogar331@gmail.com>
2020-09-30 13:53:50 -0600

    Creando la carpeta utils y su clase utilerias

---
+++ src/utils/Utilerias.java
@@ -0,0 +1,22 @@
+
+package utils;
+
+public class Utilerias {
+    
+    public static String nombreClase = "Cliente";
+    public static String nombreProducto = "Producto";
+    public static String nombreOrden = "Item Orden";
+    
+    public static String getNombreClase(){
+        return nombreClase;
+    }
+    
+    public static String getNombreProducto(){
+        return nombreProducto;
+    }
+    
+    public static String getNombreOrden(){
+        return nombreOrden;
+    }
+    
+}
